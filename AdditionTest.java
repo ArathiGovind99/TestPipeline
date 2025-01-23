4dcbcc1c48b79773101a792af1dee80d9670a780
ArathiGovind <68321648+ArathiGovind99@users.noreply.github.com>
2025-01-23 16:27:19 +0530

    Update AdditionTest.java

--- AdditionTest.java
+++ AdditionTest.java
@@ -7,7 +7,7 @@ public class AdditionTest {
     public void testAdd() {
         assertEquals(3, Addition.add(1, 2));
         assertEquals(0, Addition.add(-1, 1));
-        assertEquals(0, Addition.add(0, 0));
+        assertEquals(9, Addition.add(4, 5));
         assertEquals(300, Addition.add(100, 200));
     }
 }
