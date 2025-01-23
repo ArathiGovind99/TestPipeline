086fa5eb363f61b33660bbdcf8bb3cc399f75fe1
ArathiGovind <68321648+ArathiGovind99@users.noreply.github.com>
2025-01-23 14:55:12 +0530

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -5,3 +5,7 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert add(100, 200) == 300
+def test_add_floats():
+    assert add(1.5, 2.5) == 4.0
+    assert add(-1.5, 1.5) == 0.0
+    assert add(0.1, 0.2) == 0.3
